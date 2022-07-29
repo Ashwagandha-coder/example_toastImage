f61871169048f1a1255426eea1bf06f9d138e74b
Ashwaganha-cader <max.cacz@yandex.ru>
2022-07-29 19:34:47 +0500

    base toast

--- app/src/main/java/com/example/example_toastimage/MainActivity.java
+++ app/src/main/java/com/example/example_toastimage/MainActivity.java
@@ -2,15 +2,34 @@ package com.example.example_toastimage;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.btn);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Context context = MainActivity.this;
+
+                Toast toast = Toast.makeText(context,"Toast wiht Image",Toast.LENGTH_SHORT);
+
+
+
+            }
+        });
     }
 }
