1a304c764a9e1505b5dc24c09fe85fea2b6c9ae4
Ashwaganha-cader <max.cacz@yandex.ru>
2022-07-29 19:43:58 +0500

    done

--- app/src/main/java/com/example/example_toastimage/MainActivity.java
+++ app/src/main/java/com/example/example_toastimage/MainActivity.java
@@ -4,8 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -27,7 +30,15 @@ public class MainActivity extends AppCompatActivity {
 
                 Toast toast = Toast.makeText(context,"Toast with Image",Toast.LENGTH_SHORT);
 
+                toast.setGravity(Gravity.CENTER,0,0);
 
+                LinearLayout layout = (LinearLayout) toast.getView();
+
+                ImageView imageView = new ImageView(context);
+                imageView.setImageResource(R.drawable.ic_launcher_background);
+
+                layout.addView(imageView,0);
+                toast.show();
 
             }
         });
